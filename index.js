062f2e805114ae696b50765d56e23a3519066692
pundirakash <pundirakash@outlook.com>
2024-01-29 09:18:45 +0530

    Added FS functions

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, this is your Node.js server!');
+  if (req.url === "/") {
+    const message = 'Hello, this is your Node.js server!';
+    
+    // Write the message to a file asynchronously
+    fs.writeFile('FileSystem/output.txt', message, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('File Content: ' + message);
+    });
+  }
 });
 
 const port = 3000;
-
 server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
